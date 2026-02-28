5bff045cfb0c645ab3bce00aa710d3cd7b164b24
Pedro Pablo Alvarez <125165982+pablo1005@users.noreply.github.com>
2026-02-28 15:18:59 -0600

    Create data_cleaning_transformation.sql

---
+++ sql/data_cleaning_transformation.sql
@@ -0,0 +1,10 @@
+-- Archivo principal de limpieza y transformación de datos
+-- Proyecto: Retail E-Commerce Analytics
+-- Autor: Pedro Pablo
+
+-- El script incluye:
+-- 1. Limpieza de registros nulos
+-- 2. Identificación de devoluciones
+-- 3. Eliminación de duplicados
+-- 4. Estandarización de tipos de datos
+-- 5. Creación de tablas auxiliares para análisis
